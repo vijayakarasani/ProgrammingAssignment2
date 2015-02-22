ceca87e8996a608ec9e921155fe2dcd5de8c1231
Vijayalakshmi Karasani <vijayakarasani@gmail.com>
2015-02-21 18:52:49 -0800

    Update cachematrix.R
    Added the functionality of makeCacheMatrix and cacheSolve functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation and there may be some benefit
+# to caching the inverse of a matrix rather than compute it repeatedly. The
+# following two functions are used to cache the inverse of a matrix.
 
-## Write a short comment describing this function
+#makeCacheMatrix creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL 
+        setMatrix <- function(y) {        
+                x <<- y 
+                m <<- NULL
+        }
+        
+        getMatrix <- function() x
+        setInverse <- function(solve) m <<- solve
+        getInverse <- function() m
+        list(setmatrix = setMatrix, getmatrix = getMatrix, setinverse = setInverse, getinverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+##  cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+##  If the inverse has already been calculated (and the matrix has not changed),
+##  then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        if(nrow(x$getmatrix())==ncol(x$getmatrix())) {   #condition to check square matrix
+                m <- x$getinverse() 
+                if(!is.null(m)){ 
+                    message("getting cached data.....")
+                    return(m)
+                }
+                y <- x$getmatrix() 
+                m <- solve(y)
+                x$setinverse(m) 
+                m   # returns the inverse matrix
+        }
+        else 
+                 print("***************** please pass a square matrix as an argument ******************")
 }
+
+## Sample run:
+## m <- matrix(data = c(1,2,3,4), nrow = 2, ncol = 2)
+## m2 <- makeCacheMatrix(m)
+## No cache in the first run
+##cacheSolve(m2)
+## Retrieving from the cache in the second run
+#cacheSolve(m2)
+##getting cached data.....
+##     [,1] [,2]
+##[1,]   -2  1.5
+##[2,]    1 -0.5
